5328a7be515fc8defce2804d859f1dc927d633db
datth3 <huudat.hust@gmail.com>
2022-03-20 16:12:24 +0700

    add my profile

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,7 +7,7 @@ import Cookie from 'js-cookie';
 import { removeCookies, setCookies } from '../utils/commonFunctions'
 import { handleFetchData, getData } from '../utils/fetchData'
 
-const NavBar = ({menus}) => {
+const NavBar = ({ menus }) => {
     const router = useRouter()
     const [state, dispatch] = useContext(DataContext);
     const [menuActive, setMemuActive] = useState('');
@@ -65,108 +65,108 @@ const NavBar = ({menus}) => {
         </li>
     }
 
-    
+
     const renderMenu = (menu, levelMenu) => {
-           // console.log(menu);
-            if(!menu || !menu.length) return <div></div>;
-            //render level 1
-            if(levelMenu == 0) {
-                return (
-                    <ul className="navbar-nav navbar-nav-scroll mx-auto">
-                        {
-                            menu.map((item1 , key1) => {
-                                const length = (item1.content || []).length; 
-                                return (
-                                    <li className={`nav-item dropdown`} key={key1}>
-                                        {
-                                            length > 0 ?
+        // console.log(menu);
+        if (!menu || !menu.length) return <div></div>;
+        //render level 1
+        if (levelMenu == 0) {
+            return (
+                <ul className="navbar-nav navbar-nav-scroll mx-auto">
+                    {
+                        menu.map((item1, key1) => {
+                            const length = (item1.content || []).length;
+                            return (
+                                <li className={`nav-item dropdown`} key={key1}>
+                                    {
+                                        length > 0 ?
                                             <a className={`nav-link dropdown-toggle ${item1.active ? 'active' : ''}`} href={item1.link} id="demoMenu" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{item1.title}</a>
-                                            : 
-                                            
-                                                <a href={item1.link} className={`nav-link dropdown-toggle ${item1.active ? 'active' : ''}`} id="demoMenu" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{item1.title}</a>
-                                            
-                                        }
-                                        {
-                                            length > 0 ?
+                                            :
+
+                                            <a href={item1.link} className={`nav-link dropdown-toggle ${item1.active ? 'active' : ''}`} id="demoMenu" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{item1.title}</a>
+
+                                    }
+                                    {
+                                        length > 0 ?
                                             renderMenu(item1.content, levelMenu + 1)
                                             : null
-                                        }
-                                        
+                                    }
+
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            )
+        } else if (levelMenu == 1) {
+            return (
+                <ul className="dropdown-menu">
+                    {
+                        (menu || []).map((item1, key1) => {
+                            const length = (item1.content || []).length;
+
+                            return (
+                                <React.Fragment key={key1}>
+                                    {
+                                        length == 0 ?
+                                            (
+                                                <li >
+
+                                                    <a href={item1.link} className={`dropdown-item ${item1.active ? 'active' : ''}`} >
+                                                        {item1.title}
+                                                    </a>
+
+                                                </li>
+                                            )
+                                            : (
+                                                <li className={`dropdown-submenu dropend`}>
+                                                    <a className={`dropdown-item dropdown-toggle ${item1.active ? 'active' : ''}`} href="https://eduport.webestica.com/#">{item1.title}</a>
+                                                    {
+                                                        renderMenu(item1.content, levelMenu + 1)
+                                                    }
+
+                                                </li>
+                                            )
+                                    }
+                                    {
+                                        item1.hasDivided ? <li> <hr className="dropdown-divider" /></li> : null
+                                    }
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                </ul>
+            )
+        } else if (levelMenu == 2) {
+            return (
+                <ul className="dropdown-menu dropdown-menu-start" data-bs-popper="none">
+                    {
+                        menu.map((item1, key1) => {
+                            return (
+                                <React.Fragment key={key1}>
+                                    <li>
+                                        <Link href={item1.link}>
+                                            <a className={`dropdown-item ${item1.active ? 'active' : ''}`} >
+                                                {item1.title}
+                                            </a>
+                                        </Link>
                                     </li>
-                                )
-                            })
-                        }
-                    </ul>
-                )
-            } else if (levelMenu == 1) {
-                return (
-                    <ul className="dropdown-menu">
-                        {
-                            (menu || []).map((item1 , key1) => {
-                                const length = (item1.content || []).length; 
-                                
-                                    return (
-                                        <React.Fragment key={key1}>
-                                            {
-                                                length == 0 ?
-                                                (
-                                                    <li > 
-                                                        
-                                                            <a href={item1.link} className={`dropdown-item ${item1.active ? 'active' : ''}`} >
-                                                                {item1.title}
-                                                            </a>
-                                                        
-                                                    </li>
-                                                )
-                                                : (
-                                                    <li className={`dropdown-submenu dropend`}>
-                                                        <a className={`dropdown-item dropdown-toggle ${item1.active ? 'active' : ''}`} href="https://eduport.webestica.com/#">{item1.title}</a>
-                                                        {
-                                                            renderMenu(item1.content, levelMenu + 1)
-                                                        }
-                                                        
-                                                    </li>
-                                                )
-                                            }
-                                            {
-                                                item1.hasDivided ?  <li> <hr className="dropdown-divider" /></li> : null
-                                            }
-                                        </React.Fragment>
-                                    )
-                            })
-                        }
-                    </ul>
-                )
-            } else if(levelMenu == 2) {
-                return (
-                    <ul className="dropdown-menu dropdown-menu-start" data-bs-popper="none">
-                        {
-                            menu.map((item1, key1) => {
-                                return (
-                                    <React.Fragment key={key1}>
-                                        <li> 
-                                            <Link href={item1.link}>
-                                                <a className={`dropdown-item ${item1.active ? 'active' : ''}`} >
-                                                    {item1.title}
-                                                </a>
-                                            </Link>
-                                        </li>
-                                        {
-                                            item1.hasDivided ?  <li> <hr className="dropdown-divider" /></li> : null
-                                        }
-                                    </React.Fragment>
-                                )
-                            })
-                        }    
-                    </ul>
-                )
-            }
-       
+                                    {
+                                        item1.hasDivided ? <li> <hr className="dropdown-divider" /></li> : null
+                                    }
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                </ul>
+            )
+        }
+
     }
 
     const menuAbc = React.useMemo(() => {
         return renderMenu(menus, 0);
-     }, [menus]);
+    }, [menus]);
     return (
         <div>
             <header className="navbar-light navbar-sticky header-static">
@@ -196,7 +196,7 @@ const NavBar = ({menus}) => {
                             } */
                                 menuAbc
                             }
-                           
+
                             <div className="nav my-3 my-xl-0 px-4 flex-nowrap align-items-center">
                                 <div className="nav-item w-100">
                                     <form className="position-relative">
@@ -206,11 +206,40 @@ const NavBar = ({menus}) => {
                                 </div>
                             </div>
 
+                            <div className="dropdown ms-1 ms-lg-0">
+                                <a className="avatar avatar-sm p-0" href="#" id="profileDropdown" role="button" data-bs-auto-close="outside" data-bs-display="static" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img className="avatar-img rounded-circle" src="/Eduport%20-%20LMS,%20Education%20and%20Course%20Theme_files/01.jpg" alt="avatar" />
+                                </a>
+                                <ul className="dropdown-menu dropdown-animation dropdown-menu-end shadow pt-3" aria-labelledby="profileDropdown">
+
+                                    <li className="px-3">
+                                        <div className="d-flex align-items-center">
+
+                                            <div className="avatar me-3">
+                                                <img className="avatar-img rounded-circle shadow" src="/Eduport%20-%20LMS,%20Education%20and%20Course%20Theme_files/01.jpg" alt="avatar" />
+                                            </div>
+                                            <div>
+                                                <a className="h6" href="#">Lori Ferguson</a>
+                                                <p className="small m-0">example@gmail.com</p>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                    </li>
+
+                                    <li><a className="dropdown-item" href="/product"><i className="bi bi-person fa-fw me-2"></i>My Profile</a></li>
+                                    <li><a className="dropdown-item bg-danger-soft-hover" href="#"><i className="bi bi-power fa-fw me-2"></i>Sign Out</a></li>
+                                    
+
+                                    
+
+                                </ul>
+                            </div>
+
                         </div>
 
 
 
-                        </div>
+                    </div>
                 </nav>
             </header>
             <div id="sticky-space"></div>
